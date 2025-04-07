67152823f060c9caa5b870d0e9066f0c6adc5d5e
Alireza Salimiyan <alirezasalimiyan98@gmail.com>
2025-04-07 20:08:44 +0330

    adding the cards and 3 card has been designed ...

---
+++ components/ui/bento-grid.tsx
@@ -0,0 +1,141 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { BackgroundGradientAnimation } from "./background-gradient-animation";
+import { Globe } from "./globe";
+import { GlobeDemo } from "./GlobeDemo";
+
+export const BentoGrid = ({
+  className,
+  children,
+}: {
+  className?: string;
+  children?: React.ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto grid max-w-7xl grid-cols-1 gap-8 md:grid-cols-5",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export const BentoGridItem = ({
+  className,
+  title,
+  description,
+  id,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
+}: {
+  className?: string;
+  title?: string | React.ReactNode;
+  description?: string | React.ReactNode;
+  id?: number;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
+}) => {
+  return (
+    <div
+      className={cn(
+        "group/bento overflow-hidden border border-white/[0.1] relative shadow-input row-span-1 flex flex-col justify-between space-y-4 rounded-3xl transition duration-200 hover:shadow-xl dark:shadow-none",
+        className
+      )}
+      style={{
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+      }}
+    >
+      <div className={`${id === 6 && "flex justify-center "} h-full `}>
+        <div className="w-full h-full absolute">
+          {img && (
+            // <Image
+            //   src={img}
+            //   alt={img}
+            //   className={cn(imgClassName, "object-cover , object-center")}
+            //   width={20}
+            //   height={20}
+            // />
+            <img
+              src={img}
+              alt={img}
+              className={cn(imgClassName, "object-cover , object-center")}
+            />
+          )}
+        </div>
+        <div
+          className={`absolute right-0 -bottom-5 ${
+            id === 5 && "w-full opacity-80"
+          }`}
+        >
+          {spareImg && (
+            <img
+              src={spareImg}
+              alt={spareImg}
+              className={"object-cover object-center w-full h-full"}
+            />
+          )}
+        </div>
+        {id === 6 && (
+          <BackgroundGradientAnimation>
+            <div className="absolute z-50 flex items-center justify-center text-white font-bold " />
+          </BackgroundGradientAnimation>
+        )}
+        <div
+          className={cn(
+            titleClassName,
+            "group-hover/bento:translate-x-2 transition duration-200 relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10 "
+          )}
+        >
+          <div className="font-sans text-sm font-extralight text-[#c1c2d3] md:text-xs lg:text-base z-10">
+            {description}
+          </div>
+          <div className="lg:text-3xl max-w-96 z-10 font-sans text-lg font-bold">
+            {title}
+          </div>
+
+          {id === 2 && <GlobeDemo />}
+          {id === 3 && (
+            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
+              <div className="flex flex-col gap-3 lg:ga-8">
+                {["react", "nextjs", "typescript"].map((item) => (
+                  <span
+                    key={item}
+                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+              </div>
+              <div className="flex flex-col gap-3 lg:ga-8">
+                <span className="py-4 px-3 rounded-lg text-center bg-[#10132e]" />
+
+                {["react2", "nextjs2", "typescript2"].map((item) => (
+                  <span
+                    key={item}
+                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+/// time : 1 : 14 : 00 to continue
